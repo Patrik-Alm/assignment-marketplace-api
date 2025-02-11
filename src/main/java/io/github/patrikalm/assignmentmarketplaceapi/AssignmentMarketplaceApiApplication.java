33c3e0f1be2d39b9eb0ab05bb849fc5133dad46d
Patrik Alm <almpatrik@yahoo.se>
2025-02-11 12:46:35 +0100

    Init commit

---
+++ src/main/java/io/github/patrikalm/assignmentmarketplaceapi/AssignmentMarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.patrikalm.assignmentmarketplaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentMarketplaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentMarketplaceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/patrikalm/assignmentmarketplaceapi/AssignmentMarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patrikalm.assignmentmarketplaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentMarketplaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
