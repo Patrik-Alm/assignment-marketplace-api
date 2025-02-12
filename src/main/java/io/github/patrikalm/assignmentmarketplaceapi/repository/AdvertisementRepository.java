00b348d13d1f0c645eab6df36a97ae5c18773140
Patrik Alm <almpatrik@yahoo.se>
2025-02-12 17:58:22 +0100

    AdvertisementRepository implemented and application.properties implemented

--- src/main/java/io/github/patrikalm/assignmentmarketplaceapi/repository/AdvertisementRepository.java
+++ src/main/java/io/github/patrikalm/assignmentmarketplaceapi/repository/AdvertisementRepository.java
@@ -3,7 +3,23 @@ package io.github.patrikalm.assignmentmarketplaceapi.repository;
 import io.github.patrikalm.assignmentmarketplaceapi.entity.Advertisement;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AdvertisementRepository extends CrudRepository {
 
-    List<Advertisement>
+    List<Advertisement> findAllAdvertisementsByUserEmail(String email);
+
+    List<Advertisement> findAllAdvertisements();
+
+    List<Advertisement> findAllAdvertisementsByUserId(String userId);
+
+    List<Advertisement> findAllAdvertisementsBeforeDateEndDate();
+
+    List<Advertisement> findAllAdvertisementsAfterDateEndDate();
+
+    void deleteAllAdvertisementsAfterDateEndDate();
+
+
+
+
 }
